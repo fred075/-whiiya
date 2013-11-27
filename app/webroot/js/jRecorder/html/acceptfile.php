038c98f77867286f98ed2ca33de880b4a103d723
Fred075 <f.ganninger@gmail.com>
2013-11-27 21:00:44 +0100

    audio saving

--- app/webroot/js/jRecorder/html/acceptfile.php
+++ app/webroot/js/jRecorder/html/acceptfile.php
@@ -1,19 +1,19 @@
 <?php
-   print_r($_GET);
-   if(!isset($_REQUEST['filename']))
+
+	if(!isset($_REQUEST['filename']))
    {
    	exit('No file');
    }
    
    $filename = $_REQUEST['filename'];
     
-   $upload_path = dirname(dirname(dirname(dirname(__FILE__)))). '/audio/bread/';
+   $upload_path = dirname(dirname(dirname(dirname(__FILE__)))). '/audio/' . $_REQUEST['filename'];
    
    if (!file_exists($upload_path)) {
    	mkdir($upload_path, 0777);
    }    
    
-   $fp = fopen($upload_path.$filename.".wav", "wb");
+   $fp = fopen($upload_path.".wav", "wb");
    
    fwrite($fp, file_get_contents('php://input'));
    
