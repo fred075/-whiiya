18cea8951ef9e62bbcf5cf06fddca2e02801c5b7
mzaidkhan <mzaidkhan@192.168.2.167>
2013-11-27 21:31:13 +0100

    acceptfile modified to accept dynamic folder and file names.

--- app/webroot/js/jRecorder/html/acceptfile.php
+++ app/webroot/js/jRecorder/html/acceptfile.php
@@ -1,26 +1,34 @@
 <?php
+   
+   $filename = $_REQUEST[filename];
 
-	if(!isset($_REQUEST['filename']))
+   if(!isset($filename))
    {
    	exit('No file');
    }
    
-   $filename = $_REQUEST['filename'];
-    
-   $upload_path = dirname(dirname(dirname(dirname(__FILE__)))). '/audio/' . $_REQUEST['filename'];
+   $pos = strrpos($filename, "/");
    
-   if (!file_exists($upload_path)) {
-   	mkdir($upload_path, 0777);
-   }    
-   
-   $fp = fopen($upload_path.".wav", "wb");
-   
-   fwrite($fp, file_get_contents('php://input'));
-   
-   fclose($fp);
-   
+   if($pos > 0)
+   {
+   	$foldername = substr($filename, 0, $pos);
+   	
+   	
+   	$upload_path = dirname(dirname(dirname(dirname(__FILE__)))). '/audio/'.$foldername.'/';
+   	 
+   	if (!file_exists($upload_path)) {
+   		mkdir($upload_path, 0777);
+   	}
+
+   	$upload_path = dirname(dirname(dirname(dirname(__FILE__)))). '/audio/'.$filename;
+   	
+   	$fp = fopen($upload_path.".wav", "wb");
+   	 
+   	fwrite($fp, file_get_contents('php://input'));
+   	 
+   	fclose($fp);   	
+   }
+     
    exit('done');
    
-
-
 ?>
